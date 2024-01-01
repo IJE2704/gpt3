d165cfe3e7e81565faf9256ff53b91a8e8616d35
merajHossain <mh1669101@gmail.com>
2024-01-01 14:47:21 +0600

    brand page done

--- src/assets/Components/brand/Brand.jsx
+++ src/assets/Components/brand/Brand.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
-import './brand.css'
+import React from "react";
+import "./brand.css";
 
 const Brand = () => {
   return (
-    <div className='gpt3__brand section__padding'>
-      <h1>brand</h1>
+    <div className="gpt3__brand section__padding">
+      <div>
+        <img src="https://i.ibb.co/Hr97Q8D/google.png" alt="google" />
+      </div>
+      <div>
+        <img src="https://i.ibb.co/98NMKS3/slack.png" alt="slack" />
+      </div>
+      <div>
+        <img src="https://i.ibb.co/DtJQ36m/atlassian.png" alt="atlasian" />
+      </div>
+      <div>
+        <img src="https://i.ibb.co/jr7njdT/dropbox.png" alt="dropbox" />
+      </div>
+      <div>
+        <img src="https://i.ibb.co/GVVgbYg/shopify.png" alt="shopify" />
+      </div>
     </div>
   );
 };
--- src/assets/containers/whatGPT3/WhatGpt3.jsx
+++ src/assets/containers/whatGPT3/WhatGpt3.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './whatgpt3.css';
+import Feature from '../../Components/feature/Feature';
 
 const WhatGpt3 = () => {
   return (
-    <div>
-      <h1>what gpt</h1>
+    <div className='gpt3__whatgpt3 section__margin' id='whpt3'>
+      <div className='gpt3__whatgpt3-feature'>
+        <Feature></Feature>
+      </div>
     </div>
   );
 };
